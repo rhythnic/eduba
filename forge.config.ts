52379edf25bfa2c0cc0f1a5d057c115c770e4b90
Monkey D Luffy <nbaroni@tuta.io>
2024-01-24 16:38:21 -0500

    Add github action for publish

--- forge.config.ts
+++ forge.config.ts
@@ -2,8 +2,9 @@ import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import MakerAppImage from "@reforged/maker-appimage";
-// import { MakerDeb } from '@electron-forge/maker-deb';
-// import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerRpm } from '@electron-forge/maker-rpm';
+import MakerDMG from "@electron-forge/maker-dmg";
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
@@ -21,14 +22,23 @@ const config: ForgeConfig = {
       // iconUrl: 'https://url/to/icon.ico',
       setupIcon: './images/icon.ico'
     }),
-    new MakerZIP({},['darwin']),
-    // new MakerRpm({}),
-    // new MakerDeb({}),
+    new MakerZIP({}, ['linux']),
+    new MakerRpm({
+      options: {
+        icon: './images/icon.png'
+      }
+    }),
+    new MakerDeb({
+      options: {
+        icon: './images/icon.png'
+      }
+    }),
     new MakerAppImage({
         options: {
           icon: './images/icon.png'
         }
-    })
+    }),
+    new MakerDMG({})
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
